5803b5c00a19863f34caa711f8707de147d6ace7
Andy Palka <andrewmpalka@gmail.com>
2016-02-22 13:38:31 -0600

    added UID REF functionality

--- Nickel!/NewBusinessViewController.swift
+++ Nickel!/NewBusinessViewController.swift
@@ -19,6 +19,9 @@ class NewBusinessViewController: UIViewController, UITextFieldDelegate, CLLocati
     let coreLocationManager = CLLocationManager()
     let appDelegate = AppDelegate()
     
+    var UID: String!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,7 +29,7 @@ class NewBusinessViewController: UIViewController, UITextFieldDelegate, CLLocati
         self.businessNameTextField.delegate = self
         coreLocationManager.delegate = self
     }
-    
+//MARK UITextField Functions
     func textFieldChecker(textField: UITextField, indicator: Int) -> Bool {
         
         if(textField.text?.isEmpty == false) {
@@ -51,13 +54,70 @@ class NewBusinessViewController: UIViewController, UITextFieldDelegate, CLLocati
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         if textFieldChecker(self.businessNameTextField, indicator: 1) && textFieldChecker(self.businessEmailTextField, indicator: 2) {
-        self.newBusinessHelper(self.businessNameTextField, email: self.businessEmailTextField, location: placePlacerholder)
-            performSegueWithIdentifier("success", sender: self)
-            self.appDelegate.reveal()
+//        self.newBusinessHelper(self.businessNameTextField, email: self.businessEmailTextField, location: placePlacerholder)
+//            self.appDelegate.reveal()
 
             return resignFirstResponder()
         }
         return resignFirstResponder()
+    }
+    func dismissKeyboard(){
+        self.setEditing(false, animated: true)
+    }
+ //MARK CK Custom Functions
+    func setUID(business: CKRecord, employee: CKRecord) {
+        let timestamp = String(NSDate.timeIntervalSinceReferenceDate())
+        let timestampParts = timestamp.componentsSeparatedByString(".")
+        UID = timestampParts[0]
+        UID.appendContentsOf(timestampParts[1])
+        business.setObject(UID, forKey: "UID")
+        employee.setObject(UID, forKey: "UID")
+        userDefaults.setValue(UID, forKey: "currentOrgUID")
+        userDefaults.setBool(true, forKey: "isAdmin:")
+    }
+    
+    func saveRecords(recordsToSave: [CKRecord]) {
+        let saveOperation = CKModifyRecordsOperation(recordsToSave: recordsToSave, recordIDsToDelete: nil)
+        
+        saveOperation.modifyRecordsCompletionBlock = { saved, deleted, error in
+            if error != nil {
+                print(error)
+            } else {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.dismissViewControllerAnimated(true, completion: { () -> Void in
+                        self.performSegueWithIdentifier("success", sender: self)
+                    })
+                })
+            }
+        }
+        publicDatabase.addOperation(saveOperation)
+    }
+    
+    //MARK: Actions
+    func createBusiness() {
+//        loadingAlert("Creating Business...", viewController: self)
+        let newBusiness = CKRecord(recordType: "Businesses")
+        let newUser = CKRecord(recordType: "Users")
+        
+        let userBizRef = CKReference.init(recordID: newBusiness.recordID, action: .None)
+        newUser.setObject(userBizRef, forKey: "UID")
+        let bizUserRef = CKReference.init(recordID: newUser.recordID, action: .None)
+        newBusiness.setObject(bizUserRef, forKey: "UID")
+        
+        
+        newBusiness.setObject(self.businessNameTextField.text, forKey: "Name")
+        newBusiness.setObject(self.businessEmailTextField.text, forKey: "Email")
+        newBusiness.setObject(self.placePlacerholder, forKey: "Location")
+        
+        
+        
+        
+        setUID(newBusiness, employee: newUser)
+        
+        userDefaults.setValue(self.businessNameTextField, forKey: "currentBizName")
+        userDefaults.setBool(true, forKey: "isEmployee")
+        
+        saveRecords([newBusiness, newUser])
     }
     
 
